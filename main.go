7264d9bc0a4b6c0162345818c18bfe2be05c0b6d
yucel <yatli5645@gmail.com>
2022-07-01 13:59:13 +0300

    .

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"studapp/api/controllers"
 	"studapp/middlewares"
 
@@ -70,17 +68,4 @@ func main() {
 	router.HandleFunc("/api/amigos/getByUserId", middlewares.SetMiddlewareJSON(controllers.GetAmigosByUserID)).Methods("GET")
 	router.HandleFunc("/api/amigos/getByCityId/{cityId}", middlewares.SetMiddlewareJSON(controllers.GetAmigosByCityID)).Methods("GET")
 	router.HandleFunc("/api/amigos/delete/{id}", middlewares.SetMiddlewareAuthentication(controllers.DeleteAmigo)).Methods("DELETE")
-
-	port := "studappdemo.herokuapp.com"
-
-	if port != "8000" {
-		port = "studappdemo.herokuapp.com"
-	}
-	fmt.Println(port)
-
-	err := http.ListenAndServe(":"+port, router)
-	if err != nil {
-		fmt.Print(err)
-	}
-
 }
